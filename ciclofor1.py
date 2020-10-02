29b1eb02c2c3a9d21fceea21dc1feebe29c3b09b
unknown <WillBrael@gmail.com>
2020-10-02 10:59:43 -0300

    Se agrego un ejemplo con ciclo for

---
+++ ciclofor1.py
@@ -0,0 +1,11 @@
+#crear ciclo for que permita ingresar 10 numeros, mostrar cuantos son numeros pares y cuantos son impares..
+par=0
+impar=0
+for i in range(10):
+    nume= int(input("Digite un numero: "))
+    if(nume%2==0):
+        par=par+1
+    else:
+        impar=impar+1
+print("La cantidad de numeros pares es: "+ str(par))
+print("La cantidad de numeros impares es: "+ str(impar))
